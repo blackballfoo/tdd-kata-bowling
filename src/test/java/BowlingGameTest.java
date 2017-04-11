cd378a2a4975138df0f44f475c887dcf377dffc3
blackballfoo <foobass@googlemail.com>
2017-04-11 12:41:11 +0100

    2nd Commit - Zero Scoring Test completed in simplest form

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,9 +1,14 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BowlingGameTest {
 
     @Test
     public void testZeroScoringGame(){
-        Game game = new Game();
+        Game g = new Game();
+        g.roll(0);
+        assertThat(g.score(), is(0));
     }
 }
