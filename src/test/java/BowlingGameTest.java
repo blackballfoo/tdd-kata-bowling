3ff4c4300a3f4c2288705f165c682b7d5910ff76
blackballfoo <foobass@googlemail.com>
2017-04-11 12:47:17 +0100

    3rd Commit - 2nd failing test when we knock down 1 pin per turn.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -8,7 +8,18 @@ public class BowlingGameTest {
     @Test
     public void testZeroScoringGame(){
         Game g = new Game();
-        g.roll(0);
+        for(int i=0; i<20; i++){
+            g.roll(0);
+        }
         assertThat(g.score(), is(0));
+    }
+
+    @Test
+    public void whenOnePinIsKnockedDownForTwentyRollsScoreShouldBeTwenty(){
+        Game g = new Game();
+        for(int i=0; i<20; i++){
+            g.roll(1);
+        }
+        assertThat(g.score(), is(20));
     }
 }
