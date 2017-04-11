eeb3ecd81274614480adeba88ddec6eb331b9816
blackballfoo <foobass@googlemail.com>
2017-04-11 13:33:37 +0100

    7th Commit - Changed code to make 3rd unit test pass for when there is a spare

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class Game {
 
-    private int score = 0;
+    private List<Integer> rolls = new LinkedList<Integer>();
 
     public void roll(int pins) {
-        score += pins;
+        rolls.add(pins);
     }
 
     public int score() {
+        int score = 0;
+        int i = 0;
+       for(int frame=0; frame<10; frame++){
+           int frameScore = rolls.get(i) + rolls.get(i+1);
+           if(frameScore==10){
+                frameScore += rolls.get(i+2);
+           }
+           score += frameScore;
+           i += 2;
+       }
         return score;
     }
 }
